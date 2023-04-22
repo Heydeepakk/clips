d2017db35471a4d43b9b3e1daa7b3bfeba9f0560
Deepak Sharma <deeepaksharma121013@gmail.com>
2023-04-22 21:11:26 +0530

    color error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
-  safelist:['bg-blue-400, bg-green-400, ng-red-400'],
+  safelist: ['bg-blue-400', 'bg-green-400', 'bg-red-400'],
   theme: {
     extend: {},
-    // screens:{
-    //   'sm':'640px',
-    // }
   },
   plugins: [],
 }
